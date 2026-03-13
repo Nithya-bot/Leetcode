8086cd0c71878e62c768004c960407455203d61f
Nithyasree Balasubramaniyam <nithyasreebalasubramaniam@gmail.com>
2026-03-14 00:30:09 +0530

    Create Recyclable and Low Fat Products.sql

---
+++ sql/SQL 50/Recyclable and Low Fat Products.sql
@@ -0,0 +1,7 @@
+Recyclable and Low Fat Products
+
+https://leetcode.com/problems/recyclable-and-low-fat-products/description/?envType=study-plan-v2&envId=top-sql-50
+
+/* Write your T-SQL query statement below */
+select product_id from Products
+where low_fats='Y' and recyclable='Y';
