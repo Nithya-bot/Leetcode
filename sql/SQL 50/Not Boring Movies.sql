691a2c69bed80e65e4a09492fee651f919f9d74b
Nithyasree Balasubramaniyam <nithyasreebalasubramaniam@gmail.com>
2026-03-15 18:26:12 +0530

    Create Not Boring Movies.sql

---
+++ sql/SQL 50/Not Boring Movies.sql
@@ -0,0 +1,5 @@
+https://leetcode.com/problems/not-boring-movies/description/?envType=study-plan-v2&envId=top-sql-50
+
+select * from Cinema
+where description!='boring' and id%2=1
+ORDER BY rating desc;
