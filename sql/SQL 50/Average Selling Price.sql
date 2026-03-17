ecdab18271fc41d4a13f28021953d0355fa9a79b
Nithyasree Balasubramaniyam <nithyasreebalasubramaniam@gmail.com>
2026-03-18 00:41:28 +0530

    Create Average Selling Price.sql

---
+++ sql/SQL 50/Average Selling Price.sql
@@ -0,0 +1,6 @@
+https://leetcode.com/problems/average-selling-price/description/?envType=study-plan-v2&envId=top-sql-50
+
+select p.product_id, ROUND(ISNULL(SUM(p.price * u.units) * 1.0/NULLIF(SUM(u.units),0),0),2) as average_price from Prices p join UnitsSold u
+on p.product_id=u.product_id
+AND u.purchase_date BETWEEN p.start_date AND p.end_date
+group by p.product_id;  
