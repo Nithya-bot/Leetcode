2c267e156ef3f0a58601069a7bc0170415a3d988
Nithyasree Balasubramaniyam <nithyasreebalasubramaniam@gmail.com>
2026-06-29 23:16:45 +0530

    Create Number of Unique Subjects Taught by Each Teacher.sql

---
+++ sql/Sorting and grouping/Number of Unique Subjects Taught by Each Teacher.sql
@@ -0,0 +1,5 @@
+https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/description/?envType=study-plan-v2&envId=top-sql-50
+  
+select teacher_id, COUNT(DISTINCT(subject_id)) as cnt
+from Teacher 
+group by teacher_id
